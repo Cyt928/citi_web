52971aff8f5596e3ed5c05ca3f23ef68c2bd0b33
chen <oncwnuE4A8UM90YQvC-z5nT0KLzg@git.weixin.qq.com>
2019-08-18 18:21:13 +0800

    change index.vue by cyt

--- citi_web/src/views/index.vue
+++ citi_web/src/views/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <el-container>
-  <el-header>
+  <el-container style="background: #f6f6f6">
+  <el-header style="margin: 0;padding: 0">
     <el-menu
       :default-active="activeIndex"
       mode="horizontal"
       @select="handleSelect"
-      active-text-color="#ffd04b">
-      <el-menu-item index="1">首页（以后改成图标显示）</el-menu-item>
+      style="position: fixed;width: 100%">
+      <el-menu-item style="margin-left: 12%" index="1">
+        <el-image>
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+      </el-menu-item>
       <el-submenu index="2">
         <template slot="title">行情展示</template>
         <el-menu-item index="2-1">选项1</el-menu-item>
@@ -15,13 +21,16 @@
       </el-submenu>
       <el-menu-item index="3">我的历史</el-menu-item>
       <el-menu-item index="4">方案跟踪</el-menu-item>
+      <div style="float: right;margin-right: 15%">
+        <el-menu-item><template slot="title"><el-avatar></el-avatar></template></el-menu-item>
+      </div>
     </el-menu>
   </el-header>
-  <el-main>
+  <el-main style="min-height:700px">
     <!-- 设置router，放入不同功能页面-->
     <router-view></router-view>
   </el-main>
-  <el-footer>Nju</el-footer>
+  <el-footer style="margin-top: auto">Nju</el-footer>
 </el-container>
 </template>
 
